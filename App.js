6e633c86f0cf75d9e83dadde84d6dd9588b47444
Jasmine Vo <jasmine@Jasmines-MacBook-Pro.local>
2018-04-19 15:45:58 -0700

    feat: use provider and create-store

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import { View } from 'react-native'
 import DeckList from './components/DeckList'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
 
 export default class App extends React.Component {
   render() {
     return (
-      <View>
-        <DeckList />
-      </View>
+      <Provider store={createStore(reducer)}>
+        <View>
+          <DeckList />
+        </View>
+      </Provider>
     )
   }
 }
