7a3f61a33936c87a7a18e10a0d179e5fd0ae8397
Jasmine Vo <jasmine@Jasmines-MacBook-Pro.local>
2018-04-19 23:26:01 -0700

    feat: add TextInput form to AddDeck component

--- components/AddDeck.js
+++ components/AddDeck.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TextInput, TouchableOpacity } from 'react-native'
 
+function SubmitBtn ({ onPress }) {
+  return (
+    <TouchableOpacity
+      onPress={onPress}>
+        <Text>SUBMIT</Text>
+    </TouchableOpacity>
+  )
+}
 class AddDeck extends Component {
+  state = {
+    title: ''
+  }
+  handleTitle = (input) => {
+    this.setState({ title: input })
+  }
+  submit = () => {
+    this.state.title === '' ?
+      alert('Please enter a title.')
+    : alert('New deck created!')
+  }
   render() {
     return (
       <View>
-        <Text>Add Deck</Text>
+        <Text>NEW DECK</Text>
+        <Text>What is the title of your new deck?</Text>
+        <TextInput
+          placeholder='Title'
+          onChangeText={this.handleTitle} />
+        <SubmitBtn onPress={this.submit} />
       </View>
     )
   }
