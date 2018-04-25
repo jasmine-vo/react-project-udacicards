3385546b6748c87ce005ccdcb6c47a07823cae29
Jasmine Vo <jasmine@Jasmines-MacBook-Pro.local>
2018-04-24 21:46:02 -0700

    feat: add score at end of quiz

--- components/Quiz.js
+++ components/Quiz.js
@@ -3,14 +3,17 @@ import { View, Text } from 'react-native'
 import { getDeck } from '../utils/api'
 import { StackNavigator } from 'react-navigation'
 import TextButton from './TextButton'
+import { round } from '../utils/helpers'
 
 class Quiz extends Component {
   state = {
+    deck: '',
     index: 0,
     question: '',
     answer: '',
     showAnswer: false,
-    numOfCards: 0,
+    score: 0,
+    showScore: false,
   }
   componentDidMount() {
     const { title } = this.props.navigation.state.params
@@ -18,9 +21,9 @@ class Quiz extends Component {
     
     getDeck(title)
       .then((deck) => this.setState({
+        deck: deck.questions,
         question: deck.questions[index].question,
         answer: deck.questions[index].answer,
-        numOfCards: deck.questions.length
       }))
   }
   toggleAnswerQuestion = () => {
@@ -30,29 +33,68 @@ class Quiz extends Component {
       this.setState({ showAnswer: false })
     : this.setState({ showAnswer: true }) 
   }
+  handleCorrect = () => {
+    const { deck, index, score } = this.state
+    const nextIndex = index + 1
+    const newScore = score + 1
+
+    if (nextIndex === deck.length) {
+      this.setState({ showScore: true })
+    } else {
+      this.setState({
+        index: nextIndex,
+        question: deck[nextIndex].question,
+        answer: deck[nextIndex].answer,
+        score: newScore,
+      })
+    }
+  }
+  handleIncorrect = () => {
+    const { deck, index } = this.state
+    const nextIndex = index + 1
+
+    if (nextIndex === deck.length) {
+      this.setState({ showScore: true })
+    } else {
+      this.setState({
+        index: nextIndex,
+        question: deck[nextIndex].question,
+        answer: deck[nextIndex].answer
+      })
+    }
+  }
   render() {
     const {
       question,
       answer,
       showAnswer,
       index,
-      numOfCards
+      deck,
+      showScore,
+      score
     } = this.state
-    console.log(numOfCards)
     return (
       <View>
         <Text>Quiz</Text>
-        <Text>{ showAnswer ? answer : question }</Text>
-        <TextButton onPress={this.toggleAnswerQuestion}>
-          { showAnswer ? 'Question' : 'Answer' }
-        </TextButton>
-        <TextButton>
-          Correct
-        </TextButton>
-        <TextButton>
-          Incorrect
-        </TextButton>
-        <Text>Card {index + 1} of {numOfCards}</Text>
+        {!showScore ?
+          <View>
+            <Text>{ showAnswer ? answer : question }</Text>
+            <TextButton onPress={this.toggleAnswerQuestion}>
+              { showAnswer ? 'Question' : 'Answer' }
+            </TextButton>
+            <TextButton onPress={this.handleCorrect}>
+              Correct
+            </TextButton>
+            <TextButton onPress={this.handleIncorrect}>
+              Incorrect
+            </TextButton>
+            <Text>Card {index + 1} of {deck.length}</Text>
+          </View>
+        : <View>
+            <Text>Score</Text>
+            <Text>{round((score / deck.length)*100, 2)}%</Text>
+          </View>
+        }
       </View>
     )
   }
