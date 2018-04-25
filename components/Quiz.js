0b35832e9e9f9026da794646fdb68609bcafce3b
Jasmine Vo <jasmine@Jasmines-MacBook-Pro.local>
2018-04-24 17:37:38 -0700

    feat: Quiz view displays question/answer

--- components/Quiz.js
+++ components/Quiz.js
@@ -2,24 +2,43 @@ import React, { Component } from 'react'
 import { View, Text } from 'react-native'
 import { getDeck } from '../utils/api'
 import { StackNavigator } from 'react-navigation'
+import TextButton from './TextButton'
 
 class Quiz extends Component {
   state = {
-    deck: ''
+    index: 0,
+    question: '',
+    answer: '',
+    showAnswer: false,
   }
   componentDidMount() {
-    const { deckId } = this.props.navigation.state.params
+    const { title } = this.props.navigation.state.params
+    const { index } = this.state
     
-    getDeck(deckId)
+    getDeck(title)
       .then((deck) => this.setState({
-        deck: deck
+        question: deck.questions[index].question,
+        answer: deck.questions[index].answer
       }))
-    console.log(this.state.deck)
+  }
+  toggleAnswerQuestion = () => {
+    const { showAnswer } = this.state
+    
+    showAnswer ?
+      this.setState({ showAnswer: false })
+    : this.setState({ showAnswer: true }) 
   }
   render() {
+    const { question, answer, showAnswer } = this.state
+    console.log(question)
+    console.log(answer)
     return (
       <View>
         <Text>Quiz</Text>
+        <Text>{ showAnswer ? answer : question }</Text>
+        <TextButton onPress={this.toggleAnswerQuestion}>
+          { showAnswer ? 'Question' : 'Answer' }
+        </TextButton>
       </View>
     )
   }
