76ac8883b72d17845f165052bf0fb250c877f306
Jasmine Vo <jasmine@Jasmines-MacBook-Pro.local>
2018-04-24 21:45:02 -0700

    feat: add round function

--- utils/helpers.js
+++ utils/helpers.js
@@ -38,3 +38,15 @@ export function formatDeckResults (results) {
     : JSON.parse(results)
 }
 
+// round function from MDN web docs
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/round#A_better_solution
+export function round(number, precision) {
+  var shift = function (number, precision, reverseShift) {
+    if (reverseShift) {
+      precision = -precision;
+    }  
+    var numArray = ("" + number).split("e");
+    return +(numArray[0] + "e" + (numArray[1] ? (+numArray[1] + precision) : precision));
+  };
+  return shift(Math.round(shift(number, precision, false)), precision, true);
+}
