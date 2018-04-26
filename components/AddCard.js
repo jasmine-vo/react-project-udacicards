9815470b20f3717f003134ba46646dd28a42f7a7
Jasmine Vo <jasmine@Jasmines-MacBook-Pro.local>
2018-04-25 23:58:24 -0700

    style: add styling to AddCard component

--- components/AddCard.js
+++ components/AddCard.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
-import { View, Text, TextInput } from 'react-native'
+import { View, Text, TextInput, StyleSheet, TouchableOpacity } from 'react-native'
 import { NavigationActions } from 'react-navigation'
 import { addCardToDeck } from '../utils/api'
 import TextButton from './TextButton'
 import { connect } from 'react-redux'
 import { receiveDecks } from '../actions'
+import { white, blue } from '../utils/colors'
 
 class AddCard extends Component {
   state = {
@@ -40,24 +41,66 @@ class AddCard extends Component {
     }
   }
   render() {
+    const { title } = this.props.navigation.state.params
+
     return (
-      <View>
-        <Text>ADD CARD</Text>
+      <View style={styles.container}>
+        <Text style={styles.title}>Add a new card for {title}</Text>
         <TextInput
+          style={styles.form}
           placeholder='Question'
           value={this.state.question}
           onChangeText={this.handleQuestion} />
         <TextInput
+          style={styles.form}
           placeholder='Answer'
           value={this.state.answer}
           onChangeText={this.handleAnswer} />
-        <TextButton onPress={this.submit}>
-          SUBMIT
-        </TextButton>
+        <TouchableOpacity style={styles.submitBtn} onPress={this.submit}>
+          <Text style={styles.submitBtnText}>Submit!</Text>
+        </TouchableOpacity>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  title: {
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    fontWeight: 'bold',
+    fontSize: 22,
+    paddingTop: 40,
+    paddingBottom: 40,
+  },
+  form: {
+    textAlignVertical: 'center',
+    fontSize: 20,
+    padding: 10,
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: blue,
+    backgroundColor: white,
+    marginBottom: 40,
+  },
+  submitBtn: {
+    backgroundColor: blue,
+    padding: 10,
+    height: 45,
+    marginLeft: 100,
+    marginRight: 100,
+  },
+  submitBtnText: {
+    color: white,
+    fontSize: 18,
+    textAlign: 'center',
+  },
+})
 
 function mapStateToProps (decks) {
   return {
