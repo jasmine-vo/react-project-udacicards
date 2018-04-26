7e0ff853d4a37d23d13fcfecd1d3ac9595abb442
Jasmine Vo <jasmine@Jasmines-MacBook-Pro.local>
2018-04-25 22:48:16 -0700

    styles: add style to AddDeck component

--- components/AddDeck.js
+++ components/AddDeck.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react'
-import { View, Text, TextInput, TouchableOpacity } from 'react-native'
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet
+} from 'react-native'
 import { connect } from 'react-redux'
 import { addDeck } from '../actions'
 import { NavigationActions } from 'react-navigation'
 import { saveDeckTitle } from '../utils/api'
+import { white, blue, lightGray, gray } from '../utils/colors'
 
 function SubmitBtn ({ onPress }) {
   return (
     <TouchableOpacity
+      style={styles.submitBtn} 
       onPress={onPress}>
-        <Text>SUBMIT</Text>
+        <Text style={styles.submitBtnText}>SUBMIT</Text>
     </TouchableOpacity>
   )
 }
@@ -43,16 +51,71 @@ class AddDeck extends Component {
   render() {
     return (
       <View>
-        <Text>NEW DECK</Text>
-        <Text>What is the title of your new deck?</Text>
-        <TextInput
-          placeholder='Title'
-          value={this.state.title}
-          onChangeText={this.handleTitle} />
-        <SubmitBtn onPress={this.submit} />
+        <View style={styles.block}></View>
+          <Text style={styles.header}>New Deck</Text>
+        <View style={styles.block}></View>
+        <View style={styles.container}>
+          <Text style={styles.question}>What is the title of your new deck?</Text>
+          <TextInput
+            style={styles.title}
+            placeholder='Title'
+            value={this.state.title}
+            onChangeText={this.handleTitle} />
+          <SubmitBtn onPress={this.submit} />
+        </View>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  block: {
+    height: 20,
+    backgroundColor: blue,
+  },
+  header: {
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    fontWeight: 'bold',
+    fontSize: 25,
+    height: 25,
+    color: white,
+    backgroundColor: blue,
+  },
+  question: {
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    fontWeight: 'bold',
+    fontSize: 22,
+    paddingTop: 40,
+  },
+  title: {
+    textAlignVertical: 'center',
+    fontSize: 20,
+    padding: 10,
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: blue,
+    backgroundColor: white,
+    marginTop: 40,
+    marginBottom: 40,
+  },
+  submitBtn: {
+    backgroundColor: blue,
+    padding: 10,
+    height: 45,
+    marginLeft: 100,
+    marginRight: 100,
+  },
+  submitBtnText: {
+    color: white,
+    fontSize: 18,
+    textAlign: 'center',
+  },
+})
 
 export default connect()(AddDeck)
