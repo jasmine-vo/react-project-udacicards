473effd393359a89968ce5ba0f3b6d68a15ea429
Jasmine Vo <jasmine@Jasmines-MacBook-Pro.local>
2018-04-26 14:58:01 -0700

    style: add styling to Quiz component

--- components/Quiz.js
+++ components/Quiz.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 import { getDeck } from '../utils/api'
 import { StackNavigator } from 'react-navigation'
 import TextButton from './TextButton'
@@ -8,6 +8,7 @@ import {
   clearLocalNotification,
   setLocalNotification
 } from '../utils/helpers'
+import { white, blue, lightGray } from '../utils/colors'
 
 class Quiz extends Component {
   state = {
@@ -84,32 +85,110 @@ class Quiz extends Component {
       showScore,
       score
     } = this.state
+    const { title } = this.props.navigation.state.params
+
     return (
-      <View>
-        <Text>Quiz</Text>
+      <View style={styles.container}>
+        <Text style={styles.title}>{title} Quiz</Text>
         {!showScore ?
           <View>
-            <Text>{ showAnswer ? answer : question }</Text>
-            <TextButton onPress={this.toggleAnswerQuestion}>
-              { showAnswer ? 'Question' : 'Answer' }
-            </TextButton>
-            <TextButton onPress={this.handleCorrect}>
-              Correct
-            </TextButton>
-            <TextButton onPress={this.handleIncorrect}>
-              Incorrect
-            </TextButton>
+            <Text style={styles.question}>{ showAnswer ? answer : question }</Text>
+            <TouchableOpacity
+              style={styles.toggleBtn}
+              onPress={this.toggleAnswerQuestion}
+            >
+              <Text style={styles.submitBtnText}>
+                { showAnswer ? 'Show Question' : 'Show Answer' }
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.correctBtn}
+              onPress={this.handleCorrect}
+            >
+              <Text style={styles.submitBtnText}>Correct</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.incorrectBtn}
+              onPress={this.handleIncorrect}
+            >
+              <Text style={styles.submitBtnText}>Incorrect</Text>
+            </TouchableOpacity>
             <Text>Card {index + 1} of {deck.length}</Text>
           </View>
         : <View>
-            <Text>Score</Text>
-            <Text>{round((score / deck.length)*100, 2)}%</Text>
-            <Text>You got {score} out of {deck.length} right!</Text>
+            <Text style={styles.subtitle}>Score</Text>
+            <Text style={styles.subtitle}>{round((score / deck.length)*100, 2)}%</Text>
+            <Text style={styles.subtitle}>You got {score} out of {deck.length} right!</Text>
           </View>
         }
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingLeft: 10,
+    paddingRight: 10,
+    backgroundColor: white,
+  },
+  title: {
+    textAlignVertical: 'center',
+    fontWeight: 'bold',
+    fontSize: 18,
+    paddingTop: 20,
+  },
+  question: {
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    fontWeight: 'bold',
+    fontSize: 22,
+    paddingTop: 40,
+    paddingBottom: 40,
+  },
+  form: {
+    textAlignVertical: 'center',
+    fontSize: 20,
+    padding: 10,
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: blue,
+    backgroundColor: white,
+    marginBottom: 40,
+  },
+  toggleBtn: {
+    padding: 10,
+    height: 45,
+    margin: 20,
+  },
+  correctBtn: {
+    backgroundColor: blue,
+    padding: 10,
+    height: 45,
+    marginLeft: 100,
+    marginRight: 100,
+    marginBottom: 10,
+  },
+  incorrectBtn: {
+    backgroundColor: lightGray,
+    padding: 10,
+    height: 45,
+    marginLeft: 100,
+    marginRight: 100,
+  },
+  submitBtnText: {
+    fontSize: 18,
+    textAlign: 'center',
+  },
+  subtitle: {
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    fontWeight: 'bold',
+    fontSize: 24,
+    paddingTop: 40,
+    paddingBottom: 40,
+  },
+})
 
 export default Quiz
